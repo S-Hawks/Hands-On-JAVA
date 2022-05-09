be14e88b659775a9b2f2856b0f2731031e280ffd
Faiaz Ibne Ahsan <faiazibneahsan0@gmail.com>
2022-05-10 02:57:24 +0600

    Add files via upload

---
+++ EvenDigitSum/src/EvenDigitSum.java
@@ -0,0 +1,17 @@
+public class EvenDigitSum {
+    public static int getEvenDigitSum(int number){
+        if(number < 0){
+            return -1;
+        }
+        int temp = 0;
+        int sum = 0;
+        while (number != 0){
+            temp = number % 10;
+            if(temp % 2 == 0){
+                sum += temp;
+            }
+            number = number / 10;
+        }
+        return sum;
+    }
+}
