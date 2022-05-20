562192933dad2eb45556a58452f30bcad113b620
Faiaz Ibne Ahsan <faiazibneahsan0@gmail.com>
2022-05-21 04:34:15 +0600

    Add files via upload

---
+++ LargestPrime/src/LargestPrime.java
@@ -0,0 +1,40 @@
+public class LargestPrime {
+    public static int getLargestPrime(int number){
+        if(number < 0){
+            return -1;
+        }
+        for(int i = number/2; i != 0; i--){
+            if(isPrime(i) && number % i == 0){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static boolean isPrime(int number){
+        if(number % 2 == 0){
+            return false;
+        }
+        for(int i = 2; i <= number / 2; i++){
+            if(number % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+//Another solution using prime factorization
+//public class LargestPrime {
+//    public static int getLargestPrime(int number) {
+//        if (number < 2) {
+//            return -1;
+//        }
+//        for (int i = number/2; i > 1; i--) {
+//            if ((number % i) == 0) {
+//                number = i;
+//            }
+//        }
+//        return number;
+//    }
+//}
