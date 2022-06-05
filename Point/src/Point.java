dfc0a9e9bca24656bf7f4a4560d67b9978ddcc01
Faiaz <faiazibneahsan0@gamil.com>
2022-06-06 03:01:22 +0600

    Point Solution

---
+++ Point/src/Point.java
@@ -0,0 +1,36 @@
+public class Point {
+    private int x;
+    private int y;
+    public Point(){
+        this(0, 0);
+    }
+    public Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+    public double distance(){
+        return  Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+    }
+    public double distance(int x, int y){
+         return Math.sqrt(Math.pow((x - getX()), 2) + Math.pow((y- getY()), 2));
+    }
+    public double distance(Point other){
+        return distance(other.x, other.y);
+    }
+}
