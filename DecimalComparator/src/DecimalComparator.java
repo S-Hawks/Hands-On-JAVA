080676a8714a7fae1de94ba4f7af1950c97e6be9
faiaz <s.hawks685@gmail.com>
2022-06-23 14:05:42 +0600

    Solved

--- DecimalComparator/src/DecimalComparator.java
+++ DecimalComparator/src/DecimalComparator.java
@@ -1,5 +1,13 @@
-//public class DecimalComparator {
-//    public static boolean areEqualByThreeDecimalPlaces(double num1, double num2){
-//        double sum = num1 + num2;
-//    }
-//}
+public class DecimalComparator{
+    public static boolean areEqualByThreeDecimalPlaces(double num1, double num2){
+        num1 = num1 * 1000;
+        num2 = num2 * 1000;
+
+        int intNum1 = (int)num1;
+        int intNum2 = (int)num2;
+        if(intNum1 == intNum2){
+            return true;
+        }
+        return false;
+    }
+}
--- DecimalComparator/src/Main.java
+++ DecimalComparator/src/Main.java
@@ -1,7 +1,5 @@
-//public class Main {
-//    public static void main(String[] args) {
-//        int myIntValue = 5;
-//        double myDoubleValue = myIntValue;
-//        System.out.println(myDoubleValue);
-//    }
-//}
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(DecimalComparator.areEqualByThreeDecimalPlaces(-3.1756, -3.1756));
+    }
+}
